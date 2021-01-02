3e30f09672068130076b84d1c31cdd01c05e0ef0
AfkProgrammer <vidriohans@gmail.com>
2021-01-02 19:27:34 +0000

    Doing my first level 1 assignment

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,28 @@
 package _01_houses;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
+	Robot bob = new Robot();
+	
 	public void run() {
-		
+		bob.hide();
+		bob.setPenColor(0,255,0);
+		bob.penDown();
+		bob.setSpeed(1000000000);
+		bob.turn(270);
+		bob.move(50);
+		bob.turn(180);
+		bob.move(50);
+		bob.turn(270);
+		bob.setPenColor(255,0,0);
+		bob.move(100);
+		bob.turn(90);
+		bob.move(50);
+		bob.turn(90);
+		bob.move(100);
+		bob.turn(270);
+		bob.setPenColor(0,255,0);
+		bob.move(50);
 	}
 }
