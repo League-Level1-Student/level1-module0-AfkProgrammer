41aaa48c5569b1cb4c1d3ac879e05ea7b01ada74
AfkProgrammer <vidriohans@gmail.com>
2021-02-20 19:25:58 +0000

    Did good progress

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -8,36 +8,57 @@ public class Houses {
 	Robot bob = new Robot();
 	
 	public void run() {
+		setup();
+	
 		String housie = JOptionPane.showInputDialog(null, "What size do you want your house to be? Small, medium, or large?");
+
+		
 		if (housie.equalsIgnoreCase("small")) {
 		drawCasita (); 
 		}
-		bob.hide();
-		bob.setPenColor(0,255,0);
-		bob.penDown();
-		bob.setSpeed(1000000000);
-		bob.turn(270);
-		bob.move(50);
-		bob.turn(180);
-		bob.move(50);
-		bob.turn(270);
-		bob.setPenColor(255,0,0);
-		for(int i = 0; i <= 10; i++) {
-			drawHouse();
-		}
-		bob.setPenColor(0,255,0);
-		bob.move(50);
-		}
-
-	private void drawHouse() {
-		bob.move(100);
-		bob.turn(90);
-		bob.move(50);
-		bob.turn(90);
-		bob.move(100);
-		bob.turn(270);
 	}
+    private void setup() {
+		bob.penDown();
+		bob.hide();
+		bob.setSpeed(1000000000);
+	}
+//		bob.setPenColor(0,255,0);
+//		bob.turn(270);
+//		bob.move(50);
+//		bob.turn(180);
+//		bob.move(50);
+//		bob.turn(270);
+//		bob.setPenColor(255,0,0);
+//		for(int i = 0; i <= 10; i++) {
+//			drawHouse();
+//		}
+//		bob.setPenColor(0,255,0);
+//		bob.move(50);
+//		}
+//
+//	private void drawHouse() {
+//		bob.move(100);
+//		bob.turn(90);
+//		bob.move(50);
+//		bob.turn(90);
+//		bob.move(100);
+//		bob.turn(270);
+//	}
 	private void drawCasita () {
-		//bob.
+		bob.setPenColor(0,255,0);
+		bob.turn(270);
+		bob.move(20);
+		bob.turn(180);
+		bob.move(20);
+		bob.turn(90);
+		bob.setPenColor(255,0,0);
+		bob.move(45);
+		bob.turn(90);
+		bob.move(20);
+		bob.turn(90);
+		bob.move(45);
+		bob.setPenColor(0,255,0);
+		bob.turn(270);
+		bob.move(20)
 	}
 }
--- src/_01_houses/HousesRunner.java
+++ src/_01_houses/HousesRunner.java
@@ -1,8 +1,7 @@
 package _01_houses;
 
 public class HousesRunner {
-
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		//Here we call the run() method from the Houses class
 		Houses houseBuilder = new Houses();
 		houseBuilder.run();
